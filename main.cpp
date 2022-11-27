e769e2ca52f1d13004aee5aca08988cdd0f12671
codernineteen <wjsdPcks34@gmail.com>
2022-11-27 19:59:11 +0900

    main은 프로그램이 완성된 이후 가장 상위의 객체들을 생성하고 런타임을 담당하는 공간

--- main.cpp
+++ main.cpp
@@ -1,34 +1,14 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-
-void printASCII()
-{
-    std::string line = "";
-    std::ifstream inFile;
-    inFile.open("cards.txt");
-    if(inFile.is_open())
-    {
-        while(getline(inFile,line))
-        {
-            if(line == "n")
-            {
-                std::cout << "seperation here" << std::endl;
-            }
-            std::cout << line << std::endl;
-        }
-    }
-    else
-    {
-        std::cout << "Can't open file" << std::endl;
-    }
-    inFile.close();
-
-}
-
+#include "Deck.h"
+#include "UserInterface.h"
 
 int main()
 {
-    printASCII();
+    //카드 덱 생성
+    Deck tempDeck = Deck();
+    UserInterface UI = UserInterface();
+
+    UI.PrintCard(tempDeck);
+
     return 0;
 }
