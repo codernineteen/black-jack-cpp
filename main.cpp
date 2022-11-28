064b7bfb1e8fb365fc15e847a8b6dd061ce3f82d
codernineteen <wjsdPcks34@gmail.com>
2022-11-28 11:04:28 +0900

    테스트 코드 추가.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
-#include <iostream>
 #include "Deck.h"
 #include "UserInterface.h"
+#include "Player.h"
 
 int main()
 {
     //카드 덱 생성
     Deck tempDeck = Deck();
     UserInterface UI = UserInterface();
+    Player player1 = Player();
 
-    UI.PrintCard(tempDeck);
+    //카드 두 개 생성
+    player1.SetACard(tempDeck);
+    player1.SetACard(tempDeck);
+
+    UI.Greeting();
+    UI.ShowMenu();
+    // UI.PrintCard(player1); 잘 출력되는 것 확인함.
 
     return 0;
 }
