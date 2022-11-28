905ebb6f1eca710aff2ee31b66ed3f9b2903b678
codernineteen <wjsdPcks34@gmail.com>
2022-11-28 11:01:24 +0900

    script: 블랙잭 클래스 생성(최상위 클래스) 블랙잭 클래스는 하위 클래스의 정보를 토대로 게임의 전체적인 과정을 진행하고, 게임의 상태와 규칙을 관리한다.

---
+++ BlackJack.h
@@ -0,0 +1,18 @@
+//
+// Created by 전예찬 on 2022/11/27.
+//
+
+#ifndef BLACKJACK_BLACKJACK_H
+#define BLACKJACK_BLACKJACK_H
+
+//게임의 규칙 및 상태 관리
+//runtime용 : game logic을 모두 여기로 옮기고 하나의 함수를 main에 넘겨서 실행하기
+class BlackJack {
+    int betRange[2];
+
+public:
+    BlackJack();
+};
+
+
+#endif //BLACKJACK_BLACKJACK_H
